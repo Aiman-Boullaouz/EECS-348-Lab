c695132d9f9d3b86d0b32f22de6503bc10627a32
Aiman-Boullaouz <aaboullaouz@gmail.com>
2025-01-31 15:26:31 -0600

    Correcting syntax error

--- hello.c
+++ hello.c
@@ -19,7 +19,7 @@ int main() {
     printf("The capitalized string is: %s\n", str);
 
     // Reverse the string
-    for (i = 0;, len = strlen(str); i < len / 2; i++) {
+    for (i = 0, len = strlen(str); i < len / 2; i++) {
         char temp = str[i];
         str[i] = str[len -1 - i];
         str[len - 1 - i] = temp;
